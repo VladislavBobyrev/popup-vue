46a1fde5b20162f7a561881f7fa492811c3704df
Vladislav Bobyrev <vladislavbobyrev@yandex.ru>
2022-10-19 16:01:27 +0600

    :test_tube: добавил тесты

---
+++ src/modules/main-page/helpers/helpers.ts
@@ -0,0 +1,8 @@
+export function increment(current:number, max = 10)
+{
+  if (current < max)
+  {
+    return current + 1;
+  }
+  return current;
+}
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,14 +1,37 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import AppPopup from '@/modules/app-popup/components/AppPopup.vue';
+import { popup } from '@/modules/app-popup/hooks/app-popup.hook';
+import { increment } from '@/modules/main-page/helpers/helpers';
 
-describe('HelloWorld.vue', () =>
+describe('increment', () =>
 {
-  it('renders props.msg when passed', () =>
+  test('increments the current number 1', () =>
   {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    expect(increment(0, 10)).toBe(1);
+  });
+
+  test('does not inctement the current number over the max', () =>
+  {
+    expect(increment(10, 10)).toBe(10);
+  });
+
+  test('has a default max of 10', () =>
+  {
+    expect(increment(10)).toBe(10);
+  });
+});
+
+describe('Vue component', () =>
+{
+  test(' AppPopup', () =>
+  {
+    // отрисовываем компонент в изоляции
+    const wrapper = shallowMount(AppPopup);
+
+    // устанавливаем пропсы
+    wrapper.setProps({ popup });
+
+    // проверяем, что  popup отобразился
+    expect(wrapper);
   });
 });
